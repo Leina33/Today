3e87196854f4865c5e8c66c5f898345685c42882
Leina33 <dominickrutto@gmail.com>
2019-09-30 09:08:42 +0300

    this js script will alert the user of the details he enter in the form

--- Js/scripts.js
+++ Js/scripts.js
@@ -8,14 +8,23 @@ $(document).ready(function(){
     $("h5").click(function(){
         $(".r").toggle();
     });
-   /* $(".work1").hover(function(){
-      $(".work1").fadeOut();
-    });
-    $(".work2").mouseleave(function(){
-      $(".work2").fadeTo();
-    });*/
+
     $(".image").hover(function(){
       $(this).animate({opacity: "0.4"});
+    });
+  });
+
+  $(document).ready(function(){
+    $("#contacts").submit(function(event){
+      var name = document.getElementById(".name").val;
+      var email = document.getElementById(".email").val();
+      if (name && email != ''){
+        alert("Thank you " +  name  +  " your message is well receive");
+      }
+      else{
+        alert("invalid inputs!!");
+      }
+      event.preventDefault();
     });
   });
 
